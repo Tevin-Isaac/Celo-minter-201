e1275c2cc22eee53afdae87b64366606e75166fb
= <Emmanuelsumeh@gmail.com>
2022-06-23 12:07:13 +0100

    fix: minting issues

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,4 +1,3 @@
-
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
@@ -6,110 +5,109 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NFT.sol";
 
 
-contract NFTMarket is ReentrancyGuard, Ownable {
+contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemId;
     Counters.Counter private _itemsSold;
 
-    address payable contractOwner;
-    uint256 public mintingCost = 0 ether;
+    address payable owner;
+    uint256 public mintingCost = 0.0001 ether;
 
     constructor(){
-        contractOwner = payable(msg.sender);
+        owner = payable(msg.sender);
     }
 
     enum ListingStatus {
-      Active,
-      Sold,
-      Cancelled
+        Active,
+        Sold,
+        Cancelled
     }
 
     struct _Item {
-      ListingStatus status;
-      address nftContract;
-      address payable owner;
-      address payable creator;
-      uint256 token;
-      uint256 price;
+        ListingStatus status;
+        address nftContract;
+        address payable owner;
+        address payable creator;
+        uint256 token;
+        uint256 price;
     }
 
     event Item (
-      address nftContract,
-      address owner,
-      address creator,
-      uint256 token,
-      uint256 price
+        address nftContract,
+        address owner,
+        address creator,
+        uint256 token,
+        uint256 price
     );
 
     event CancelSell(
-      uint256 token,
-      address owner
-	  );
+        uint256 token,
+        address owner
+    );
 
     event Sold(
-      address nftContract,
-      address owner,
-      address creator,
-      uint256 token,
-      uint256 price
-	  );
+        address nftContract,
+        address owner,
+        address creator,
+        uint256 token,
+        uint256 price
+    );
 
     mapping(uint => _Item) public Items;
 
     mapping(address => uint) createdPerWallet;
     mapping(address => uint) ownedPerWallet;
 
-   
+    bool isPaused;
 
-    function sellItem(string memory uri,uint256 _price,address _nftContract) public payable nonReentrant{
-      require(_price > 0, "Price must be at least 1 wei");
-      require(msg.value == mintingCost, "You need to pay minting price");
-      require(_nftContract != address(0), "Enter a valid marketplace address");
-      require(bytes(uri).length > 0, "Enter a valid uri");
+    function sellItem(string memory uri,uint256 _price,address _nftContract) public payable notPaused nonReentrant{
+        require(_price > 0, "Price must be at least 1 wei");
+        require(msg.value == mintingCost, "You need to pay minting price");
+        require(_nftContract != address(0), "Enter a valid marketplace address");
+        require(bytes(uri).length > 0, "Enter a valid uri");
 
-      uint256 itemId = _itemId.current();
-      _itemId.increment();
-      createdPerWallet[msg.sender]++;
-      uint256 _tokenId = NFT(_nftContract).safeMint(uri,address(this),msg.sender);
+        uint256 itemId = _itemId.current();
+        _itemId.increment();
+        createdPerWallet[msg.sender]++;
+        uint256 _tokenId = NFT(_nftContract).safeMint(uri,address(this),msg.sender);
 
-      Items[itemId] =  _Item(
-          ListingStatus.Active,
-          _nftContract,
-          payable(address(this)),
-          payable(msg.sender),
-          _tokenId,
-          _price
-      );
+        Items[itemId] =  _Item(
+            ListingStatus.Active,
+            _nftContract,
+            payable(address(this)),
+            payable(msg.sender),
+            _tokenId,
+            _price
+        );
 
-      IERC721(_nftContract).transferFrom(msg.sender, address(this), _tokenId);
+        IERC721(_nftContract).transferFrom(msg.sender, address(this), _tokenId);
 
-      emit Item(
-        _nftContract,
-        payable(address(this)),
-        payable(msg.sender),
-        _tokenId,
-        _price
-      );
+        emit Item(
+            _nftContract,
+            payable(address(this)),
+            payable(msg.sender),
+            _tokenId,
+            _price
+        );
 
     }
 
-    function cancelSell(uint256 _tokenId) public isValidTokenId(_tokenId)  {
-      _Item storage listedItem = Items[_tokenId];
-      require(msg.sender == listedItem.owner || msg.sender == listedItem.creator, "Only owner can cancel listing");
-		  require(listedItem.status == ListingStatus.Active, "Listing is not active");
+    function cancelSell(uint256 _tokenId) public isValidTokenId(_tokenId) notPaused {
+        _Item storage listedItem = Items[_tokenId];
+        require(msg.sender == listedItem.owner || msg.sender == listedItem.creator, "Only owner can cancel listing");
+        require(listedItem.status == ListingStatus.Active, "Listing is not active");
 
-      listedItem.status = ListingStatus.Cancelled;
-	    IERC721(listedItem.nftContract).transferFrom(address(this), msg.sender, listedItem.token);
+        listedItem.status = ListingStatus.Cancelled;
+        IERC721(listedItem.nftContract).transferFrom(address(this), msg.sender, listedItem.token);
 
-      emit CancelSell(listedItem.token,listedItem.owner);
+        emit CancelSell(listedItem.token,listedItem.owner);
     }
 
-    function buyItem(uint256 _tokenId) public payable isValidTokenId(_tokenId) nonReentrant {
-      _Item storage listedItem = Items[_tokenId];
+    function buyItem(uint256 _tokenId) public payable isValidTokenId(_tokenId) notPaused nonReentrant {
+        _Item storage listedItem = Items[_tokenId];
 
         require(listedItem.price == msg.value, 'Price must be equal to NFT price');
 
@@ -120,7 +118,7 @@ contract NFTMarket is ReentrancyGuard, Ownable {
         ownedPerWallet[msg.sender]++;
         address payable ownerAddress = listedItem.creator;
         if(listedItem.owner == address(0)){
-          ownerAddress = listedItem.owner;
+            ownerAddress = listedItem.owner;
         }
         (bool success,) = ownerAddress.call{value: msg.value}("");
         require(success, "Transfer of payment failed");
@@ -129,94 +127,108 @@ contract NFTMarket is ReentrancyGuard, Ownable {
         IERC721(listedItem.nftContract).transferFrom(address(this), msg.sender, listedItem.token);
 
         //Pay owner of the NFT
-        (bool sent, ) = payable(contractOwner).call{value: mintingCost}("");
+        (bool sent, ) = payable(owner).call{value: mintingCost}("");
         require(sent, "Transfer of mintCost failed");
 
         emit Sold(
-          listedItem.nftContract,
-          msg.sender,
-          listedItem.creator,
-          listedItem.token,
-          listedItem.price
+            listedItem.nftContract,
+            msg.sender,
+            listedItem.creator,
+            listedItem.token,
+            listedItem.price
         );
 
     }
 
-     // Fetch all unsold items
+    // Fetch all unsold items
     function fetchMarketItems() public view returns (_Item[] memory) {
-      uint itemCount = _itemId.current();
-      uint unsoldItemCount = _itemId.current() - _itemsSold.current();
-      uint currentIndex = 0;
+        uint itemCount = _itemId.current();
+        uint unsoldItemCount = _itemId.current() - _itemsSold.current();
+        uint currentIndex = 0;
 
-      _Item[] memory items = new _Item[](unsoldItemCount);
-      for (uint i = 0; i < itemCount; i++) {
-        if (Items[i].owner == address(this) && Items[i].status == ListingStatus.Active) {
-          _Item storage currentItem = Items[i];
-          items[currentIndex] = currentItem;
-          currentIndex += 1;
+        _Item[] memory items = new _Item[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if (Items[i].owner == address(this) && Items[i].status == ListingStatus.Active) {
+                _Item storage currentItem = Items[i];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
         }
-      }
-      return items;
+        return items;
     }
 
     // Fetch creator NFT's
     function fetchCreatorItemsListed() public view hasCreatedItems returns (_Item[] memory) {
-      uint totalItemCount = _itemId.current();
-      uint itemCount = createdPerWallet[msg.sender];
-      _Item[] memory items = new _Item[](itemCount);
-      uint currentIndex = 0;
+        uint totalItemCount = _itemId.current();
+        uint itemCount = createdPerWallet[msg.sender];
+        _Item[] memory items = new _Item[](itemCount);
+        uint currentIndex = 0;
 
-      for(uint i = 0; i < totalItemCount; i++) {
-        if(Items[i].creator == msg.sender){
-          _Item storage currentItem = Items[i];
-          items[currentIndex] = currentItem;
-          currentIndex += 1;
+        for(uint i = 0; i < totalItemCount; i++) {
+            if(Items[i].creator == msg.sender){
+                _Item storage currentItem = Items[i];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
         }
-      }
 
-      return items;
+        return items;
     }
 
 
     // Fetch owner NFT's
     function fetchOwnerItemsListed() public view hasOwnerItems  returns (_Item[] memory) {
 
-      uint totalItemCount = _itemId.current();
-      uint itemCount = ownedPerWallet[msg.sender];
-      uint currentIndex = 0;
-      _Item[] memory items = new _Item[](itemCount);
+        uint totalItemCount = _itemId.current();
+        uint itemCount = ownedPerWallet[msg.sender];
+        uint currentIndex = 0;
+        _Item[] memory items = new _Item[](itemCount);
 
-      for (uint i = 0; i < totalItemCount; i++) {
-        if(Items[i].owner == msg.sender){
-          _Item storage currentItem = Items[i];
-          items[currentIndex] = currentItem;
-          currentIndex += 1;
+        for (uint i = 0; i < totalItemCount; i++) {
+            if(Items[i].owner == msg.sender){
+                _Item storage currentItem = Items[i];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
         }
-      }
 
-      return items;
+        return items;
     }
 
-  
+    function pause() public onlyOwner {
+        isPaused = true;
+    }
+
+    function unPause() public onlyOwner {
+        isPaused = false;
+    }
 
 
     modifier hasCreatedItems {
-      require(createdPerWallet[msg.sender] > 0, "You have not yet created any items");
-      _;
+        require(createdPerWallet[msg.sender] > 0, "You have not yet created any items");
+        _;
     }
 
     modifier hasOwnerItems {
-      require(ownedPerWallet[msg.sender] > 0, "You currently own no items");
-      _;
+        require(ownedPerWallet[msg.sender] > 0, "You currently own no items");
+        _;
     }
 
     modifier isValidTokenId(uint _tokenId) {
-    require(_tokenId >= 0, "Enter a valid tokenId");
-    _;
+        require(_tokenId >= 0, "Enter a valid tokenId");
+        _;
     }
 
-//     pause all minting and selling actions
-  
+    //     pause all minting and selling actions
+    modifier notPaused() {
+        require(!isPaused, "Contract is paused");
+        _;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
 
 
 }
--- redux/interactions.js
+++ redux/interactions.js
@@ -4,24 +4,26 @@ import NFTMarketContract from "../artifacts/contracts/NFTMarket.sol/NFTMarket.js
 import NFTContract from "../artifacts/contracts/NFT.sol/NFT.json"
 import axios from "axios";
 
-var marketPlaceAddress = process.env.NFT_MARKET_CONTRACT_ADDRESS
-var nftAddress = process.env.NFT_CONTRACT_ADDRESS
+// var marketPlaceAddress = process.env.NFT_MARKET_CONTRACT_ADDRESS
+var marketPlaceAddress = "0x3B93f48aB3F02Ae1522D179aF3440FeFBEE8690f"
+// var nftAddress = process.env.NFT_CONTRACT_ADDRESS
+var nftAddress = "0xD98Cb02aA84Cd452af430538cF036FC72207Ffc4"
 
 export const formatNFTData = async(data,nftContract) =>{
-  const tokenUri = await nftContract.tokenURI(data.token)
-  const meta = await axios.get(tokenUri)
+    const tokenUri = await nftContract.tokenURI(data.token)
+    const meta = await axios.get(tokenUri)
 
-  const formattedData = {
-    name:meta.data.name,
-    image:meta.data.image,
-    desc:meta.data.description,
-    price:weiToEther(data.price),
-    token:(data.token).toString(),
-    creator:data.creator,
-    attributes:meta.data.attributes
-  }
+    const formattedData = {
+        name:meta.data.name,
+        image:meta.data.image,
+        desc:meta.data.description,
+        price:weiToEther(data.price),
+        token:(data.token).toString(),
+        creator:data.creator,
+        attributes:meta.data.attributes
+    }
 
-  return formattedData;
+    return formattedData;
 
 }
 
@@ -30,124 +32,125 @@ export const weiToEther = (num) =>{
 }
 
 export const etherToWei = (n) => {
-  const weiBigNumber = ethers.utils.parseEther(n.toString());
-  const wei = weiBigNumber.toString();
-  return wei
+    const weiBigNumber = ethers.utils.parseEther(n.toString());
+    const wei = weiBigNumber.toString();
+    return wei
 }
 
- export const loadWeb3 = async(dispatch)=>{
+export const loadWeb3 = async(dispatch)=>{
     if(window.ethereum){
-    const provider = new ethers.providers.Web3Provider(window.ethereum,"any")
+        const provider = new ethers.providers.Web3Provider(window.ethereum,"any")
 
-    dispatch(actions.web3Loaded(provider))
-    const addresses = await provider.listAccounts(); 
-    dispatch(actions.walletAddressLoaded(addresses[0]))
-    return provider;
+        dispatch(actions.web3Loaded(provider))
+        const addresses = await provider.listAccounts();
+        dispatch(actions.walletAddressLoaded(addresses[0]))
+        return provider;
     }
-  }
-  
-  export const loadContracts = async(provider,dispatch) =>{
+}
 
-    const { chainId } = await provider.getNetwork()
-    if(chainId !== 44787){
-      return {
-        marketplace:null,
-        nft:null
-      }
-    }
+export const loadContracts = async(provider,dispatch) =>{
+
+    // const { chainId } = await provider.getNetwork()
+    // if(chainId !== process.env.CHAIN_ID){
+    //   return {
+    //     marketplace:null,
+    //     nft:null
+    //   }
+    // }
 
     var signer ;
-    const addresses = await provider.listAccounts(); 
+    const addresses = await provider.listAccounts();
     if(addresses.length > 0){
-      signer = provider.getSigner()
+        signer = provider.getSigner()
     }else{
-      signer = provider
+        signer = provider
     }
+    console.log({addresses})
     const nftMarketplaceContract = new ethers.Contract(marketPlaceAddress, NFTMarketContract.abi, signer);
     const nftContract = new ethers.Contract(nftAddress, NFTContract.abi, signer);
 
     dispatch(actions.nftMarketplaceContractLoaded(nftMarketplaceContract))
     dispatch(actions.nftContractLoaded(nftContract))
-    
+
     return {
         marketplace:nftMarketplaceContract,
         nft:nftContract
     }
 
-  }
+}
 
-  export const loadAccount = async(provider,dispatch) =>{
+export const loadAccount = async(provider,dispatch) =>{
     if(!provider){
-      return
+        return
     }
     const signer = await provider.getSigner();
     const connectedWallet = await signer.getAddress();
     dispatch(actions.walletAddressLoaded(connectedWallet))
-  }
+}
 
-  export const loadUnsoldNFT = async(provider,marketplaceContract,nftContract,dispatch)=>{
+export const loadUnsoldNFT = async(provider,marketplaceContract,nftContract,dispatch)=>{
 
-    const { chainId } = await provider.getNetwork()
-    if(chainId !== 44787){
-      dispatch(actions.unsoldNFTLoaded([]))
-      return 
-    }
+    // const { chainId } = await provider.getNetwork()
+    // if(chainId !== process.env.CHAIN_ID){
+    //   dispatch(actions.unsoldNFTLoaded([]))
+    //   return
+    // }
 
     var unsoldNft = await marketplaceContract.fetchMarketItems()
     const formattedNFTList = await Promise.all(unsoldNft.map(nft=>{
-      var res = formatNFTData(nft,nftContract)
-      return res;
+        var res = formatNFTData(nft,nftContract)
+        return res;
     }))
 
     dispatch(actions.unsoldNFTLoaded(formattedNFTList))
-  }
+}
 
-  export const loadMintedNFT = async(provider,marketplaceContract,account,nftContract,dispatch)=>{
+export const loadMintedNFT = async(provider,marketplaceContract,account,nftContract,dispatch)=>{
 
-    const { chainId } = await provider.getNetwork()
-    if(!account || chainId !== 44787){
-      dispatch(actions.mintedNFTLoaded([]))
-      return
-    }
+    // const { chainId } = await provider.getNetwork()
+    // if(!account || chainId !== 44787){
+    //   dispatch(actions.mintedNFTLoaded([]))
+    //   return
+    // }
     var unsoldNft = await marketplaceContract.fetchCreatorItemsListed({from:account})
-    
+
     const formattedNFTList = await Promise.all(unsoldNft.map(nft=>{
-      var res = formatNFTData(nft,nftContract)
-      return res;
+        var res = formatNFTData(nft,nftContract)
+        return res;
     }))
 
     dispatch(actions.mintedNFTLoaded(formattedNFTList))
-  }
+}
 
-  export const loadOwnedNFT = async(provider,marketplaceContract,account,nftContract,dispatch)=>{
-    
-    const { chainId } = await provider.getNetwork()
-    if(!account || chainId !== 44787){
-      dispatch(actions.ownedNFTLoaded([]))
-      return 
-    }
+export const loadOwnedNFT = async(provider,marketplaceContract,account,nftContract,dispatch)=>{
+
+    // const { chainId } = await provider.getNetwork()
+    // if(!account || chainId !== process.env.CHAIN_ID){
+    //   dispatch(actions.ownedNFTLoaded([]))
+    //   return
+    // }
     var unsoldNft = await marketplaceContract.fetchOwnerItemsListed({from:account})
-    
+
     const formattedNFTList = await Promise.all(unsoldNft.map(nft=>{
-      var res = formatNFTData(nft,nftContract)
-      return res;
+        var res = formatNFTData(nft,nftContract)
+        return res;
     }))
 
     dispatch(actions.ownedNFTLoaded(formattedNFTList))
-  }
+}
 
 
-  export const buyNFT = async(marketplaceContract,account,tokenId,price,dispatch,onSuccess,onError) =>{
+export const buyNFT = async(marketplaceContract,account,tokenId,price,dispatch,onSuccess,onError) =>{
     if(!account){
-      return
+        return
     }
     try {
-      var res = await marketplaceContract.buyItem(tokenId,{value:etherToWei(price),from:account})
-      const receipt = await res.wait();
-      onSuccess()
-      dispatch(actions.nftPurchased(tokenId))
-      console.log(receipt)
+        var res = await marketplaceContract.buyItem(tokenId,{value:etherToWei(price),from:account})
+        const receipt = await res.wait();
+        onSuccess()
+        dispatch(actions.nftPurchased(tokenId))
+        console.log(receipt)
     } catch (error) {
-      onError(error.message)
+        onError(error.message)
     }
-  }
+}
